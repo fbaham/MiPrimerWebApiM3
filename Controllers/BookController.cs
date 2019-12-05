ab2e7deeef844dcbfd54db766abed063fea6561f
Francisco Bahamondes Castro <fbahamondesc@icafal.cl>
2019-12-05 18:07:24 -0300

    BookController

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,7 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using MiPrimerWebApiM3.Models;
+
 namespace MiPrimerWebApiM3.Controllers
 {
-    public class BookController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
     {
-        
+        public BookController()
+        {
+        }
+
+        // GET api/book
+        [HttpGet("")]
+        public ActionResult<IEnumerable<string>> Getstrings()
+        {
+            return new List<string> { };
+        }
+
+        // GET api/book/5
+        [HttpGet("{id}")]
+        public ActionResult<string> GetstringById(int id)
+        {
+            return null;
+        }
+
+        // POST api/book
+        [HttpPost("")]
+        public void Poststring(string value)
+        {
+        }
+
+        // PUT api/book/5
+        [HttpPut("{id}")]
+        public void Putstring(int id, string value)
+        {
+        }
+
+        // DELETE api/book/5
+        [HttpDelete("{id}")]
+        public void DeletestringById(int id)
+        {
+        }
     }
 }
