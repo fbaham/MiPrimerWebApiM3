268d634d16ca1d4c601471228aebccc2be16b988
Francisco Bahamondes Castro <fbahamondesc@icafal.cl>
2019-12-06 09:26:04 -0300

    test

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,9 @@ namespace MiPrimerWebApiM3
             var connection = new SqliteConnection("DataSource=MyDb.sqlite");
             connection.Open();
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connection));
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options => options.SerializerSettings
+                    .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
